1944687af49eedc9a93fcd5fd8ad97caa89401d9
Bobervg <sp1rityc@gmail.com>
2020-09-21 16:00:07 +0300

    added project to git

---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return (
+      <div className='content'>
+      <div>
+      <img src='https://www.metoffice.gov.uk/binaries/content/gallery/metofficegovuk/hero-images/advice/maps-satellite-images/satellite-image-of-globe.jpg'></img>
+      </div>
+      <div className='avatar'>
+        ava+desc
+        <img src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"></img>
+      </div>
+      <div>
+        Myposts
+        <div> 
+          New post
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+      Main content
+  </div>
+    )
+}
+export default Profile;
